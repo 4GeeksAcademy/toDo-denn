56dc3c77e2987fe1729d14cc3fabd567a08ea85a
denn-p <dennielysp@gmail.com>
2026-01-05 18:13:50 +0000

    primer commit del todo

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,26 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
+import "./HomeStyles.css";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
-//create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-            
+	const [task, setTask] = useState("")
+	const [tasks, setTasks] = useState([])
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+
+	const handleClick = (e) => {
+		e.preventDefault();
+		if (task.trim() === '') {
+			alert('Please enter a valid task.');
+			return;
+		}
+		setTasks([...tasks, task])
+		setTask('')
+	}
+
+
+	const deleteFuction = (indexToDelete) => {
+		const newTasks = tasks.filter((task, index) => index !== indexToDelete)
+		setTasks(newTasks)
+	}
+
+
+	return (
+		<div>
+			<h1 className="Title">What do we do today?</h1>
+
+				<div className="d-flex justify-content-center">
+
+					<form onSubmit={handleClick}>
+
+						<input
+							type="text"
+							value={task}
+							onChange={(e) => setTask(e.target.value)}
+							name="task"
+							placeholder="Enter your task here">
+						</input>
+
+						<button className="addButton" onClick={handleClick}> add</button>
+
+					</form>
+
+				</div>
+
+
+			<div className="card">
+				<ul>
+					{
+						tasks.map((task, index) =>
+							<li key={index}> {task} <button className="deleteButton" onClick={() => deleteFuction(index)}>X</button></li>)
+					}
+				</ul>
+			</div>
+
 		</div>
 	);
 };
